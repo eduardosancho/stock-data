6693e3c84c03bb17480319d15beb85582330860d
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-08 11:12:51 -0600

    Remove boilerplate files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
   faCheckSquare, faCoffee, faBars, faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import { Counter } from './features/counter/Counter';
-import logo from './logo.svg';
 import NavigationBar from './features/navbar/NavigationBar';
 import Home from './features/home/Home';
 import StockPrice from './features/stockPrice/StockPrice';
@@ -26,56 +24,6 @@ function App() {
           <Route path="/details" element={<Details />} />
         </Routes>
       </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,
-          <span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 });
 
