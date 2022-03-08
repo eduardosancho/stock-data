2b4be2e226ab066ddb99eb1c23620ed9bdccd3d9
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-08 10:32:38 -0600

    export default store, linter fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './app/store';
+import store from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
