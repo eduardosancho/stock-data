5d9eabc2491258c8762eea41e46475a0b1042f6e
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-08 11:03:02 -0600

    Add a navbar using React Router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheckSquare, faCoffee, faBars, faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { Counter } from './features/counter/Counter';
 import logo from './logo.svg';
 import NavigationBar from './features/navbar/NavigationBar';
+import Home from './features/home/Home';
+import StockPrice from './features/stockPrice/StockPrice';
+import Details from './features/details/Details';
 import './App.css';
 
 library.add(fab, faCheckSquare, faCoffee, faBars, faUser);
@@ -13,7 +18,14 @@ library.add(fab, faCheckSquare, faCoffee, faBars, faUser);
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stockPrice" element={<StockPrice />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
--- src/features/navbar/NavigationBar.js
+++ src/features/navbar/NavigationBar.js
@@ -1,30 +1,56 @@
 import { NavLink } from 'react-router-dom';
 import { Container, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../../logo.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavigationBar = () => {
-  const exclamation = '!';
+  const Links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/stockPrice',
+      text: 'Stock Price',
+    },
+    {
+      id: 3,
+      path: '/details',
+      text: 'Details',
+    },
+  ];
 
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <FontAwesomeIcon icon="bars" size="xl" color="#fff" />
-        <Navbar.Brand href="#home">
-          <img
-            alt="logo"
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{' '}
-          Stock Data
-        </Navbar.Brand>
+        <NavLink to="/" className="links" activeclassname="active-link">
+          <FontAwesomeIcon icon="bars" size="xl" color="#fff" />
+        </NavLink>
+        <NavLink to="/" className="links" activeclassname="active-link">
+          <Navbar.Brand to="/">
+            <img
+              alt="logo"
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' '}
+            Stock Data
+          </Navbar.Brand>
+        </NavLink>
+        {Links.map((link) => (
+          <NavLink to={link.path} className="links" activeclassname="active-link" key={link.id}>
+            {link.text}
+          </NavLink>
+        ))}
         <FontAwesomeIcon icon="user" size="xl" color="#fff" />
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 export default NavigationBar;
