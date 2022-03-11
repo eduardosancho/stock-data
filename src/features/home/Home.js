b03397be4a5701548dc094b1ca094b2280953e60
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-11 13:52:03 -0600

    home now always displays both forms

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import CardGroup from 'react-bootstrap/CardGroup';
 import StockCard from '../stockCard/StockCard';
 import {
   selectStatusHome,
-  fetchCurrentStock,
   selectResultData,
 } from './homeActions';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,22 +11,20 @@ import DateForm from '../forms/DateForm';
 import SearchForm from '../forms/SearchForm';
 
 const Home = () => {
-  const dispatch = useDispatch();
   const resultData = useSelector(selectResultData);
   const status = useSelector(selectStatusHome);
 
-  useEffect(() => {
-    if (resultData.length === 0) {
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/MSFT?apikey=52fdd430ffd03a27128580af9ddc7381'));
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=52fdd430ffd03a27128580af9ddc7381'));
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/GOOGL?apikey=52fdd430ffd03a27128580af9ddc7381'));
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/AMZN?apikey=52fdd430ffd03a27128580af9ddc7381'));
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/TSLA?apikey=52fdd430ffd03a27128580af9ddc7381'));
-      dispatch(fetchCurrentStock('https://financialmodelingprep.com/api/v3/quote-short/FB?apikey=52fdd430ffd03a27128580af9ddc7381'));
-    }
-  }, []);
-
-  if (status === 'loading' || status === 'idle') return <p>Loading...</p>;
+  if (status === 'loading' || status === 'idle') {
+    return (
+      <>
+        <DateForm />
+        <p>Loading...</p>
+        <footer>
+          <SearchForm />
+        </footer>
+      </>
+    );
+  }
 
   return (
     <>
