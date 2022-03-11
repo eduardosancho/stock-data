9c3885362c201334e4f7164c96f00b440351914f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-11 00:26:55 -0600

    display data from fetch on details section

--- src/features/details/Details.js
+++ src/features/details/Details.js
@@ -14,15 +14,71 @@ const Details = () => {
   if (status === 'loading' || status === 'idle') return <p>Loading...</p>;
 
   return (
-    <ListGroup>
-      <ListGroup.Item>
-        {companyRating.symbol}
-      </ListGroup.Item>
-      <ListGroup.Item>Ipsum</ListGroup.Item>
-      <ListGroup.Item>Ipsum</ListGroup.Item>
-      <ListGroup.Item>Ipsum</ListGroup.Item>
-      <ListGroup.Item>Ipsum</ListGroup.Item>
-    </ListGroup>
+    <>
+      <h1>{companyRating.symbol}</h1>
+      <h3>{companyRating.date}</h3>
+      <h5>
+        Rating 1-5:
+        {'\n'}
+        {companyRating.ratingScore}
+      </h5>
+      <ListGroup>
+        <ListGroup.Item>
+          DCF Score:
+          {'\n'}
+          {companyRating.ratingDetailsDCFScore}
+          <br />
+          DCF Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsDCFRecommendation}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          ROE Score:
+          {'\n'}
+          {companyRating.ratingDetailsROEScore}
+          <br />
+          ROE Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsROERecommendation}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          ROA Score:
+          {'\n'}
+          {companyRating.ratingDetailsROAScore}
+          <br />
+          ROA Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsROARecommendation}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          DE Score:
+          {'\n'}
+          {companyRating.ratingDetailsDEScore}
+          <br />
+          DE Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsDERecommendation}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          PE Score:
+          {'\n'}
+          {companyRating.ratingDetailsPEScore}
+          <br />
+          PE Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsPERecommendation}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          PB Score:
+          {'\n'}
+          {companyRating.ratingDetailsPBScore}
+          <br />
+          PB Recommendation:
+          {'\n'}
+          {companyRating.ratingDetailsPBRecommendation}
+        </ListGroup.Item>
+      </ListGroup>
+    </>
   );
 };
 
