335aee592b962bcbc4be9c3d61a4b02b6b9fbeb9
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-03-11 16:16:11 -0600

    fix, date offset of one day

--- src/features/utils.js
+++ src/features/utils.js
@@ -3,10 +3,10 @@ const today = () => {
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  if (month < 10 && day < 10) return `${year}-0${month}-0${day}`;
-  if (month < 10 && day > 10) return `${year}-0${month}-${day}`;
-  if (month > 10 && day < 10) return `${year}-${month}-0${day}`;
-  return `/${year}-${month}-${day}`;
+  if (month < 10 && day < 10) return `${year}-0${month}-0${day - 1}`;
+  if (month < 10 && day > 10) return `${year}-0${month}-${day - 1}`;
+  if (month > 10 && day < 10) return `${year}-${month}-0${day - 1}`;
+  return `${year}-${month}-${day - 1}`;
 };
 
 export default today;
