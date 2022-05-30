97710a1022acabedbd1f7b0f58baffa76dd4f738
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-30 16:59:45 -0600

    Add search form for any company with abbreviation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,24 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  fetchCurrentStock
+} from '../redux/homeActions';
+import apikey from '../apiKey';
 
 const SearchForm = () => {
-  const currentYear = new Date().getFullYear();
-  const years = [];
-  for (let i = 2000; i <= currentYear; i += 1) {
-    years.push(i);
-  }
+  const dispatch = useDispatch();
+
+  const [company, setCompany] = useState('');
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    dispatch(fetchCurrentStock(`https://financialmodelingprep.com/api/v3/quote-short/${company}?apikey=${apikey}`));
+  };
 
   return (
-    <Form className="d-flex flex-column border-1 mx-auto w-100 justify-content-center gap-2">
+    <Form onSubmit={submitSearch} className="d-flex flex-column border-1 mx-auto w-100 justify-content-center gap-2">
       <Form.Label>
         Search for a specific company
         <br />
@@ -18,7 +27,15 @@ const SearchForm = () => {
         <em>Apple =&gt; AAPL</em>
       </Form.Label>
       <Form.Group controlId="formCompanyName" className="d-flex justify-content-center gap-3">
-        <Form.Control type="text" placeholder="Amazon => AMZN" className="py-2 w-50" />
+        <Form.Control
+          type="text"
+          placeholder="Amazon => AMZN"
+          className="py-2 w-50"
+          value={company}
+          onChange={({ target }) => {
+            setCompany(target.value);
+          }}
+        />
         <Button type="submit" className="align-self-center">
           Search
         </Button>
